10c29f7f9fa4e5d38632ecc00832ddb49c782e4e
Abhijith v <abhijithababhijith@gmail.com>
2022-01-11 09:21:42 +0530

    dockerize node

--- node-js/index.js
+++ node-js/index.js
@@ -2,12 +2,11 @@ const express = require('express');
 
 const app = express()
 
-
 app.get('/', (req, res) => {
     return res.status(200).send("Hello World From Express !!")
 })
 
 
-app.listen(3000, () => {
-    console.log('app started !! on port:', 3000)
+app.listen(process.env.PORT || 3000, () => {
+    console.log('app started !! on port:', process.env.PORT || 3000)
 })
