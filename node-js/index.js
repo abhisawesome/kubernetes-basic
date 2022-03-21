e1138f0dfb6675851f308682a9062ba116f2db13
Abhijith v <abhijithababhijith@gmail.com>
2022-03-21 07:51:19 +0530

    added some api

--- node-js/index.js
+++ node-js/index.js
@@ -6,6 +6,21 @@ app.get('/', (req, res) => {
     return res.status(200).send("Hello World From Express !!")
 })
 
+app.get('/get', (req, res) => {
+    const { throwError } = req.query;
+    if (throwError) {
+        return res.status(500).send("Error !!")
+    }
+    return res.status(200).send({ ...req.query })
+})
+
+app.post('/post', (req, res) => {
+    const { throwError } = req.query;
+    if (throwError) {
+        return res.status(500).send("Error !!")
+    }
+    return res.status(200).send({ ...req.body })
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('app started !! on port:', process.env.PORT || 3000)
